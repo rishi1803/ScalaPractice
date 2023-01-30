6a324ecd058c0576b21214a5706d59c4a3f2a4ee
Rishabh <rishi.1803@gmail.com>
2023-01-30 21:49:06 +0530

    problem solving hacker rank

--- src/main/scala/com/DSAUdemy/LinkedListProblem.scala
+++ src/main/scala/com/DSAUdemy/LinkedListProblem.scala
@@ -2,14 +2,26 @@ package com.DSAUdemy
 
 object LinkedListProblem extends App {
 
-  val a = List(1,2,3)
-  val head = a.head
-  val tail = a.tail
-  val b = List(head,tail)
+//  val a = List(1,2,3)
+//  val head = a.head
+//  val tail = a.tail
+//
+//  val b = List(head,tail)
+//
+//  b.foreach(println)
 
-  b.foreach(println)
+  val c = scala.collection.mutable.LinkedList(1,2,3,4,5,6)
+  println(c.next)
 
-  val c = scala.collection.mutable.LinkedList(1,2)
+//  var slowPtr = c.head
+  val abc = c.length -2 -1
+  val par = c(abc)
+
+  println(c(abc))
+//  def deleteKthElement(lst: scala.collection.mutable.LinkedList[Int], pos:Int) = {
+//
+//  }
+//  }
 
 
 }
